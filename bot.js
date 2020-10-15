e3d2ffa06e27ee6369812549240dc68b875aaf87
Joshua Pregbaha <jpregbaha@gmail.com>
2020-10-15 14:25:56 +0100

    Tweet about Ending SARS and support others tweeting about it

--- bot.js
+++ bot.js
@@ -6,15 +6,15 @@ console.log("working");
 
  ******************* */
 
- const express = require('express');
- const app = express();
- app.set('port', (process.env.PORT || 5000));
- app.get('/', function (request, response) {
- 	var result = 'App is running'
- 	response.send(result);
- }).listen(app.get('port'), function () {
- 	console.log('App is running, server is listening on port ', app.get('port'));
- });
+const express = require('express');
+const app = express();
+app.set('port', (process.env.PORT || 5000));
+app.get('/', function (request, response) {
+	var result = 'App is running'
+	response.send(result);
+}).listen(app.get('port'), function () {
+	console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 
 /* ******************
@@ -30,56 +30,58 @@ const Twitter = require('./twitter'); //import statement
  Key variables
 
  ******************* */
- const me = {
- 	id: 2725238596,
- 	screen_name: 'JPregbaha'
- };
+const me = {
+	id: 2725238596,
+	screen_name: 'JPregbaha'
+};
 
- const botofaweirdo = {
- 	id: 1265455946442117000,
- 	screen_name: 'botofaweirdo'
- };
+const botofaweirdo = {
+	id: 1265455946442117000,
+	screen_name: 'botofaweirdo'
+};
 
- const emojis = ['👊', '🙌', '👍', '👌', '👯', '🥰', '😁'];
+const emojis = ['👊', '🙌', '👍', '👌', '👯', '🥰', '😁'];
 
 /* ******************
 
  General Functions
 
  ******************* */
- function shouldSendReply() {
- 	const randomNumber = Math.random();
- 	if (randomNumber > 0.2) return true;
- 	return false;
- }
+function shouldSendReply() {
+	const randomNumber = Math.random();
+	if (randomNumber > 0.2) return true;
+	return false;
+}
 
- function getEmoji() {
- 	return emojis[Math.floor(Math.random() * emojis.length)];
- }
+function getEmoji() {
+	return emojis[Math.floor(Math.random() * emojis.length)];
+}
 
- function getTweet(tweet) {
- 	const text = `You are appreciated ${ getEmoji() }`;
- 	return text;
- }
+function getTweet(tweet) {
+	const text = `You are appreciated ${ getEmoji() }`;
+	return text;
+}
 
- function getPromotionTweet(tweet) {
- 	const text = `Hi there,${ getEmoji() }. Want to read some cool stories and maybe some weird thoughts? Visit joshuapregbaha.wordpress.com`;
- 	return text;
- }
+function getPromotionTweet(tweet) {
+	const text = `Hi there,${ getEmoji() }. Want to read some cool stories and maybe some weird thoughts? Visit joshuapregbaha.wordpress.com`;
+	return text;
+}
 
- function getReplyToMeTweet(tweet) {
- 	const text = 'Leave me alone';
- 	return text;
- }
+function getReplyToMeTweet(tweet) {
+	const text = 'Leave me alone';
+	return text;
+}
 
 // Setting up a tweet stream
-const stream = T.stream('statuses/filter', { track: ['joshuapregbaha.wordpress.com', '@botofaweirdo'] });
+const stream = T.stream('statuses/filter', {
+	track: ['joshuapregbaha.wordpress.com', '@botofaweirdo']
+});
 
 // Anytime someone tweets about my blog
 stream.on('tweet', tweetEvent);
 
 function tweetEvent(tweet) {
-	if ( tweet.user.id === botofaweirdo.id ) {
+	if (tweet.user.id === botofaweirdo.id) {
 		console.log("This is me");
 		return;
 		console.log("I'm talking to myself");
@@ -87,21 +89,21 @@ function tweetEvent(tweet) {
 
 	Twitter.like(tweet);
 
-	if ( tweet.user.id === me.id ) {
-		if ( tweet.text.toLowerCase().includes('@botofaweirdo') && !(tweet.text.toLowerCase().includes('via')) ) {
-			if ( shouldSendReply() ) {
+	if (tweet.user.id === me.id) {
+		if (tweet.text.toLowerCase().includes('@botofaweirdo') && !(tweet.text.toLowerCase().includes('via'))) {
+			if (shouldSendReply()) {
 				Twitter.reply(tweet, getReplyToMeTweet(tweet));
 			}
 			return;
-		}		
+		}
 		Twitter.retweet(tweet);
 		return;
 	}
 
-	if ( tweet.retweeted_status ) return;
+	if (tweet.retweeted_status) return;
 
-	if ( tweet.text.toLowerCase().includes('@botofaweirdo') && !(tweet.text.toLowerCase().includes('via')) ) {
-		if ( shouldSendReply() ) {
+	if (tweet.text.toLowerCase().includes('@botofaweirdo') && !(tweet.text.toLowerCase().includes('via'))) {
+		if (shouldSendReply()) {
 			Twitter.reply(tweet, getPromotionTweet(tweet));
 		}
 		return;
@@ -112,12 +114,12 @@ function tweetEvent(tweet) {
 
 
 
-//Tweet once everyday
-var htmlToJson = require('html-to-json'); // import module to convert the html home page of my blog to json
+//Tweet twice everyday
+const htmlToJson = require('html-to-json'); // import module to convert the html home page of my blog to json
 
-var url = process.env.blog_url; // the url of my blog home page
+const url = process.env.blog_url; // the url of my blog home page
 
-var linkParser = htmlToJson.createParser(['a[title]', {
+const linkParser = htmlToJson.createParser(['a[title]', {
 	'text': function ($a) {
 		return $a.attr('title');
 	},
@@ -133,21 +135,23 @@ linkParser.request(url).done(function (links) {
 	links = JSON.stringify(links);
 	fs.writeFile('posts.json', links, (err) => {
 		if (err) throw err;
-	  	console.log('The file has been saved!');
+		console.log('The file has been saved!');
 	});
 }); // create a json file with the titles and links of the posts
 
-var posts = require('./posts.json'); // get the json file created from the html home page of my blog
+const posts = require('./posts.json'); // get the json file created from the html home page of my blog
 
-function random_from_array(posts){
+function random_from_array(posts) {
 	return posts[Math.floor(Math.random() * posts.length)]; // pick a random post
 }
 
-var post = random_from_array(posts);
+const post = random_from_array(posts);
 
 if (post.text != 'To the top') {
 	setInterval(function tweetSomething() {
-		var tweet = {status : post.text + ' ' + post.href}
+		const tweet = {
+			status: post.text + ' ' + post.href
+		}
 
 		T.post('statuses/update', tweet, tweeted);
 
@@ -156,7 +160,58 @@ if (post.text != 'To the top') {
 				console.log(err);
 			} else {
 				console.log('It worked!');
-			}  
+			}
 		}
-	}, 1000*60*60*12); // tweet a random post every 12 hours
+	}, 1000 * 60 * 60 * 12); // tweet a random post every 12 hours
+}
+
+
+///////////////END SARS///////////////
+
+const endSarsTweets = ['Educational and Economy reform', 'National constitution Reform', 'Debt accountability', 'Security Reform', 'Anti-people policies cancellation', 'Restructuring', 'Save cost of governance'];
+
+const endSarsPost = random_from_array(endSarsTweets);
+
+setInterval(function tweetAboutSars() {
+	const tweet = {
+		status: endSarsPost + '#EndSARS #EndPoliceBrutality'
+	};
+
+	T.post('statuses/update', tweet, tweeted);
+
+	function tweeted(err, data, response) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log('It worked!');
+		}
+	}
+}, 1000 * 60 * 60 * 2);
+
+const endSarsStream = T.stream('statuses/filter', {
+	track: ['#EndSARS', '#EndPoliceBrutality']
+});
+
+function shouldInteract() {
+	const randomNumber = Math.floor(Math.random() * 10) + 1;
+	if (randomNumber > 8) return true;
+	return false;
+}
+
+// Anytime someone tweets about my ending sars or ending police brutality
+endSarsStream.on("tweet", tweetSarsEvent);
+
+function tweetSarsEvent(tweet) {
+	if (tweet.user.id === botofaweirdo.id) {
+		console.log("This is me");
+		return;
+		console.log("I'm talking to myself");
+	}
+
+	if (shouldInteract()) {
+		Twitter.like(tweet);
+		Twitter.retweet(tweet);
+	}
+
+	if (tweet.retweeted_status) return;
 }
